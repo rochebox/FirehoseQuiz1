332f9806ec8bcf5a453b6ea08b4bef6ad97f56a7
rochebox <rochebox@gmail.com>
2016-06-30 20:13:24 +0000

    Final stylings and adding background

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Quiz1
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #Add fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    #Do precompile
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
